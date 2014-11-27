6ccdecadb83b54179ddd3c204cead7271b1b277e
Colombio <vsprem@HRVZ1MC002.local>
2014-11-27 23:15:27 +0100

    1st Commit

---
+++ Colombio/Colombio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colombio
+//
+//  Created by Vlatko Šprem on 27/11/14.
+//  Copyright (c) 2014 Colombio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Colombio/Colombio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Colombio
+//
+//  Created by Vlatko Šprem on 27/11/14.
+//  Copyright (c) 2014 Colombio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
