332556455f0a5bfa6ba2d6f88f2e92c7d04732f5
Colombio <vsprem@HRVZ1MC002.local>
2014-11-29 00:34:15 +0100

    Project setup 2

---
+++ Colombio/Colombio/CustomClasses/Database/Database.h
@@ -0,0 +1,37 @@
+//
+//  Database.h
+//  
+//
+//  Created by Matija Kraljić on 01/12/13.
+//  Copyright (c) 2013 m2m. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface Database : NSObject{
+	sqlite3 *db;
+}
+//@property (nonatomic, readwrite)sqlite3 *db;
+@property (readwrite) BOOL isEmptyDB;
+
+
+-(void)CreateEmptyDB;
+-(void)UpgradeDB;
+
+- (id)getColForSQL:(NSString *)sql;
+- (id)getColumnForSQL:(NSString *)sql;
+- (NSMutableArray *)getAllForSQL:(NSString *)sql;
+- (NSMutableDictionary *)getRowForSQL:(NSString *)sql;
+- (void)updateSQL:(NSString *)sql forTable:(NSString *)table;
+
+- (int)getCountWhere:(NSString *)where forTable:(NSString *)table;
+- (int)getMax:(NSString *)key Where:(NSString *)where forTable:(NSString *)table;
+- (int)getSum:(NSString *)key Where:(NSString *)where forTable:(NSString *)table;
+
+-(BOOL)insertDictionaryWithoutColumnCheck:(NSDictionary *)dbData forTable:(NSString *)table;
+- (void)checkTableforColumns:(NSDictionary *)dbData forTable:(NSString*)table;
+- (BOOL)updateDictionary:(NSDictionary *)dbData forTable:(NSString *)table where:(NSString *)where;
+
+-(void)execute:(NSString*)sql;
+@end
