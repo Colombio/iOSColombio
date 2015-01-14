806a8f8faf252104a8499e7aefbf2e1cf7942a53
Colombio <vsprem@HRVZ1MC002.local>
2015-01-14 22:59:13 +0100

    ContainerVC

---
+++ Colombio/Colombio/VIewControllers/ContainerViewController/ContainerViewController.h
@@ -0,0 +1,15 @@
+//
+//  ContainerViewController.h
+//  Colombio
+//
+//  Created by Vlatko Šprem on 13/01/15.
+//  Copyright (c) 2015 Colombio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContainerViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *viewHeader;
+@property (weak, nonatomic) IBOutlet UIView *viwContainer;
+@end
---
+++ Colombio/Colombio/VIewControllers/ContainerViewController/ContainerViewController.m
@@ -0,0 +1,29 @@
+//
+//  ContainerViewController.m
+//  Colombio
+//
+//  Created by Vlatko Šprem on 13/01/15.
+//  Copyright (c) 2015 Colombio. All rights reserved.
+//
+
+#import "ContainerViewController.h"
+
+@interface ContainerViewController ()
+
+@end
+
+@implementation ContainerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
